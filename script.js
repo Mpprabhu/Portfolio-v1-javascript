c7a9fb4259e9b570417f50e39aee972624ea0650
Mpprabhu <prabhu92004@gmail.com>
2024-07-11 18:38:01 +0530

    Portfolio -- upto projects

---
+++ script.js
@@ -0,0 +1,42 @@
+const header = document.querySelector(".header");
+const aboutSection = document.querySelector(".about-section");
+const allSections = document.querySelectorAll(".section");
+
+// Sticky Navigation
+const headerHeight = header.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    document.body.classList.add("sticky");
+  } else {
+    document.body.classList.remove("sticky");
+  }
+};
+
+const observer = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerHeight}px`,
+});
+
+observer.observe(aboutSection);
+
+// Section Revealer
+const revealElement = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealElement, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  // section.classList.add("section-hidden");
+});

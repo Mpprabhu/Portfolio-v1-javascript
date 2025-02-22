8778b53884818eeef5b9739f81cc032f34dad4df
Mpprabhu <prabhu92004@gmail.com>
2025-02-23 01:17:05 +0530

    projects updation

--- JS/projects.js
+++ JS/projects.js
@@ -11,7 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
         (project) => `
       <div class="project-card">
         <div class="project-img-box">
-          <img src="${project.image}" alt="${project.subheading}" class="project-img" />
+          <img src="${project.image}" alt="${
+          project.subheading
+        }" class="project-img" />
+          <a href="${project.githubLink}" target="_blank" class="${
+          project.githubLink ? "github-btn" : "none"
+        }">
+            <i class="ph ph-github-logo"></i> View on GitHub
+          </a>
         </div>
         <div class="project-content">
           <h3 class="project-title">${project.title}</h3>

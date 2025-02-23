dff25417c2b289c676e987bcd1037d306f8110b7
Mpprabhu <prabhu92004@gmail.com>
2025-02-23 22:16:33 +0530

    queries fixes

--- JS/common.js
+++ JS/common.js
@@ -42,17 +42,25 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn-contact");
+const contactForm = document.querySelector(".cta-form");
+
 const openModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
+
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+  if (contactForm) {
+    contactForm.reset();
+  }
 };
+
 btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
